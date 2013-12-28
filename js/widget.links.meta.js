385aca0cfcdf06e55b6033f4e1d437477005034c
astux7 <Asta.bevainyte@gmail.com>
2013-12-28 14:00:33 +0000

    extracting title and meta

--- js/widget.links.meta.js
+++ js/widget.links.meta.js
@@ -1,13 +1,20 @@
-function parse_data(data){
-	console.log(data)
+function parse_data(data, url){
+	var output = Array(url);
   var pattern_description = /name="description"\scontent="(.+?)"/;  
-  var pattern_logo = /property="og:image"\scontent="(.+?)"/;   
-  get_description(data,pattern_logo);
-  get_description(data,pattern_description)
-}
-function get_description(info, pattern){
-  var matches = info.join(' ').match(pattern);
-  console.log(matches[1]);
+  var pattern_logo = /property="og:image"\scontent="(.+?)"/;  
+  var pattern_title =  /<title>(.+?)<\/title>/i;  
+  output[1] = get_description(data, pattern_logo);
+  output[2] = get_description(data, pattern_description);
+  output[3] = get_description(data, pattern_title);
+  console.log(data);
+
+  $("#profile-widget").html('<a href="'+output[0]+'"><img width="25%" height="25%" src="'+output[1]+'" /><br />'+output[3]+": <br />  "+output[2]+'</a>');
 }
 
-jsonlib.scrape('head meta', 'https://github.com/astux7', function(data) { parse_data(data); });
+function get_description(info, pattern){
+  var matches = info.join(' ').match(pattern);
+  return matches[1];
+}
+
+var url =  'https://github.com/astux7'
+jsonlib.scrape('html head', url, function(data) { parse_data(data, url); });
